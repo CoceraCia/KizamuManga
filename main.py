8d7a2436c8fb794ddd19072081c15397a00f22be
CoceraCia <coceraciamiguel@gmail.com>
2025-07-10 13:57:26 +0200

    cleaner, and it just misses downloading the mangas to cbz

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
 from utils import WebScrapping
 
+
 def main():
-    ws = WebScrapping()
-    manga_name = input("Search for a manga->")
-    mangas_result_set = ws.search_manga(manga_name)
-    selected_manga = int(input("Select one of the mangas->"))
-    manga_url = ws.select_manga(mangas_result_set, selected_manga)
-    manga_list = ws.show_chapters(manga_url)
-    print(manga_url)
+    ws = WebScrapping(verify=False)
+    try:
+        # Retrieve mangas by title
+        title = input("Input manga title->")
+        mangas_retrieved = ws.get_mangas_by_title(title)
+        print("AVAILABLE MANGAS:")
+        for i, key in enumerate(mangas_retrieved.keys(), start=0):
+            print(f"{i} - {key}")
+            
+        # User selects the manga
+        n = int(input("Select one of the mangas, just the number->"))
+        for i, value in enumerate(mangas_retrieved.values(), start=0):
+            if i == n:
+                href = value
+                break
+        print(href)
+        
+        # Retrieve all the chapters
+        chapters = ws.get_chapters_by_mangaurl(href)
+        
+        print("AVAILABLE CHAPTERS")
+        for key in chapters.keys():
+            print(f"- {key}")
+
+        download_all = True if str(input("Download all chapters? y/n"))  == "y" else False
+        
+        if download_all is True:
+            None
+            
+        ws.close()
+    except Exception as e:
+        print(e)
+        ws.close()
+
+
 if __name__ == "__main__":
     main()
--- utils/web_scrapping.py
+++ utils/web_scrapping.py
@@ -3,68 +3,58 @@ from bs4 import BeautifulSoup
 from playwright.sync_api import sync_playwright
 
 BASE_URL = "https://weebcentral.com"
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/114.0.0.0 Safari/537.36'}
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/114.0.0.0 Safari/537.36"
+}
+
+
 class WebScrapping:
     def __init__(self, verify=True):
         self.verify = verify
+        p = sync_playwright().start()
+        self.browser = p.chromium.launch(headless=True)
+        self.page = self.browser.new_page()
 
+    def close(self):
+        self.page.close()
+        self.browser.close()
 
-    def setup_search_page(self, url):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
-            page.goto(url)
-            page.wait_for_selector("#search-results > article:nth-child(1)")
-            html = page.content()
-            page.close()
-            return html
-        
-    def setup_manga_page(self, url):
-        with sync_playwright() as p:
-            browser = p.chromium.launch(headless=True)
-            page = browser.new_page()
-            page.goto(url)
-            page.wait_for_selector("#chapter-list > button")
-            page.click("#chapter-list > button")
-            page.wait_for_selector("#chapter-list > div:nth-child(1) > a")
-            html = page.content()
-            page.close()
-            return html
-        
-            
-    
-    def search_manga(self, title: str):
+    def get_mangas_by_title(self, title: str):
         title = title.replace(" ", "+")
         url = f"{BASE_URL}/search?text={title}&sort=Best+Match&order=Descending&official=Any&anime=Any&adult=Any&display_mode=Full+Display"
-        html = self.setup_search_page(url)
-        
+        self.page.goto(url)
+        self.page.wait_for_selector("#search-results > article:nth-child(1)")
+        html = self.page.content()
+
         soup = BeautifulSoup(html, "html.parser")
         manga_names = soup.select("a.line-clamp-1")
-        
-        print("📖 - AVAILABLE MANGAS:")
-        for i, item in enumerate(manga_names, start=1):
-            print(f"{i}-{item.text}")
-            print(item["href"])
-    
-        return manga_names
-    
-    def select_manga(self, manga_names, num):
-          for i, item in enumerate(manga_names, start=1):
-            if i == num:
-                return item["href"]
-              
-    def show_chapters(self, manga_url):
-        html = self.setup_manga_page(manga_url)
-        
-        soup = BeautifulSoup(html, "html.parser")
-        tags = soup.find_all("a", class_="hover:bg-base-300 flex-1 flex items-center p-2")
-        
-            
-            
-        
-        
-        
 
-    def export_to_file(self, html):
-        with open("resultados.html", "w", encoding="utf-8") as f:
-            f.write(html.prettify())  # O usa str(txt)
+        nl = {}
+
+        for item in manga_names:
+            nl[item.text.strip()] = item.get("href", "N/A")
+
+        return nl
+
+    def get_chapters_by_mangaurl(self, manga_url):
+        self.page.goto(manga_url)
+        self.page.wait_for_selector("#chapter-list > button")
+        self.page.click("#chapter-list > button")
+        self.page.wait_for_timeout(1200)
+        html = self.page.content()
+
+        soup = BeautifulSoup(html, "html.parser")
+        tags = soup.find_all(
+            "a", class_="hover:bg-base-300 flex-1 flex items-center p-2"
+        )
+        
+        nl = {}
+        
+        for tag in tags:
+            href = tag.get("href", "N/A")
+            for span in tag.find_all("span"):
+                if span.has_attr("class") and span["class"] == []:
+                    nl[span.text.strip()] = href.strip()
+                    break
+
+        return nl
