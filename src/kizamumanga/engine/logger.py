fad41b52ad1f50e38ae2b67717467bc213edf49b
CoceraCia <coceraciamiguel@gmail.com>
2025-07-24 10:30:51 +0200

    RotatingFileHandler implemented

--- src/kizamumanga/engine/logger.py
+++ src/kizamumanga/engine/logger.py
@@ -2,16 +2,20 @@
 KizamuManga - Logger module for handling logging in the application."""
 import logging
 import os
+
+from logging.handlers import RotatingFileHandler
 from .config import PROJECT_ROOT
 
 class Logger:
     """Logger class to handle logging for the application."""
     def __init__(self, name: str, console: bool = False):
         self.path_logs = os.path.join(PROJECT_ROOT, "logs")
-        os.makedirs(os.path.dirname("logs/"), exist_ok=True)
+        
         self.console = console
+        
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
+        
         self._set_up_files()
         self._set_up_handlers(name)
 
@@ -19,23 +23,23 @@ class Logger:
     def _set_up_handlers(self, name: str ):
         """Set up logging handlers for different log files."""
         formater = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        errors_handler = logging.FileHandler(f"{self.path_logs}/errors.log")
+        errors_handler = RotatingFileHandler(f"{self.path_logs}/errors.log", maxBytes=5_000_000, backupCount=3)
         errors_handler.setLevel(logging.ERROR)
         errors_handler.setFormatter(formater)
         self.logger.addHandler(errors_handler)
 
         if "runner" in name:
-            app_handler = logging.FileHandler(f"{self.path_logs}/app.log")
+            app_handler = RotatingFileHandler(f"{self.path_logs}/app.log", maxBytes=5_000_000, backupCount=3)
             app_handler.setLevel(logging.DEBUG)
             app_handler.setFormatter(formater)
             self.logger.addHandler(app_handler)
         elif "downloader" in name:
-            downloader_handler = logging.FileHandler(f"{self.path_logs}/downloader.log")
+            downloader_handler = RotatingFileHandler(f"{self.path_logs}/downloader.log", maxBytes=5_000_000, backupCount=3)
             downloader_handler.setLevel(logging.INFO)
             downloader_handler.setFormatter(formater)
             self.logger.addHandler(downloader_handler)
         elif "scraping" in name:
-            scraping_handler = logging.FileHandler(f"{self.path_logs}/scraping.log")
+            scraping_handler = RotatingFileHandler(f"{self.path_logs}/scraping.log", maxBytes=5_000_000, backupCount=3)
             scraping_handler.setLevel(logging.INFO)
             scraping_handler.setFormatter(formater)
             self.logger.addHandler(scraping_handler)
