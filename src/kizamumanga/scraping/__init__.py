980c1b99868d84ba0ee8f223f11cf4f5680b6748
CoceraCia <coceraciamiguel@gmail.com>
2025-09-12 13:14:09 +0200

    me he equivocao de rama pero bueno se vuelve a subir jasdjaj

--- src/kizamumanga/scraping/__init__.py
+++ src/kizamumanga/scraping/__init__.py
@@ -1,4 +1,5 @@
 from .weeb_central import WeebCentral
 from .inmanga import InManga
+from .zona_tmo import ZonaTMO
 from .interface import ScraperInterface
 from .base import ScraperBase, MangaError
